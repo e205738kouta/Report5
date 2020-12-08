ac07afd4902bd11d920ba411c7c6d11674dfc9aa
Nomura Kouta <e205738@ie.u-ryukyu.ac.jp>
2020-12-08 20:21:00 +0900

    step3 done

--- src/jp/ac/uryukyu/ie/e205738/Main.java
+++ src/jp/ac/uryukyu/ie/e205738/Main.java
@@ -1,15 +1,15 @@
 package jp.ac.uryukyu.ie.e205738;
 
 public class Main {
-    public static void main(String args[]){
-        String str =null;
-        try{
-          System.out.println(str.length());
-        }catch(NullPointerException e){
-            System.out.println("NullPointerExceptionが発生しました。");
-            System.out.println(e.getMessage());
-        }  
+    public static void main(String args[]) {
+        /*
+         * String str =null; try{ System.out.println(str.length());
+         * }catch(NullPointerException e){
+         * System.out.println("NullPointerExceptionが発生しました。");
+         * System.out.println(e.getMessage()); } ここまでステップ１と２
+         */
+        String str = "百二十三";
+        int value = Integer.parseInt(str);
     }
 
-    
 }
